08eb75b83a8566afa82833b596015969474b8d48
stevencooper <stevencooper@sheridan.edu>
2025-02-22 10:14:18 -0700

    added credits to splash screen

--- college-map/src/components/SplashScreen.tsx
+++ college-map/src/components/SplashScreen.tsx
@@ -6,14 +6,16 @@ const SplashScreen = () => {
 
   return (
     <div className="splash-container">
-      <h1>Welcome to Sheridan College Interactive Map</h1>
-      <p>Find your way around campus with our interactive room finder</p>
+      <h1>Welcome to SCIM!</h1>
+      <p>Welcome to Sheridan College Interactive Map!</p>
+      <p>Find your way around Campus with this interactive room finder / building browser!</p>
       <button 
         className="enter-button"
         onClick={() => navigate('/buildings')}
       >
         Enter Room Finder
       </button>
+      <span className="credits">by Steven Cooper</span>
     </div>
   );
 };
